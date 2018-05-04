f2f527677fe7a8e9f1efda279184c3895e1e9234
Juan M. Avalos <avalosj177@gmail.com>
2018-05-04 20:13:42 +0000

    Lesson 14 complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,4 +7,17 @@ class PlacesController < ApplicationController
 	def new  # thi is the action in that places the form on a page called 'new'
 		@place = Place.new
 	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :desciption, :address)
+	end
+
+
 end
