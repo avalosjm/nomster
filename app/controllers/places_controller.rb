00fd3583553a1f85128d3ae35e257ec2a8c840d7
Juan M. Avalos <avalosj177@gmail.com>
2018-05-04 20:03:44 +0000

    Lesson 13 complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,8 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 	end
+
+	def new  # thi is the action in that places the form on a page called 'new'
+		@place = Place.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 	root 'places#index'     # setting up the 'places' controller
+	resources :places      # added to allow to get more URLs for a new place
 end
