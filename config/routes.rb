818fb9b42eb5315279fdf5d141685f3fc8a53900
Juan M. Avalos <avalosj177@gmail.com>
2018-05-05 00:25:38 +0000

    Lesson 16 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 	root 'places#index'     # setting up the 'places' controller
 	resources :places      # added to allow to get more URLs for a new place
 end
