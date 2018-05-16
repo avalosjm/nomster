e914d61e729f6cc289fdb72eded7e1b728077560
Juan M. Avalos <avalosj177@gmail.com>
2018-05-16 22:31:37 +0000

    1805161530

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,11 @@
 class PhotosController < ApplicationController
-	Before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def create
 		@photo = Place.find(params[:place_id])
 	end
+
+	private
 
 	def photo_params
 			params.require(:photo).permit(:caption, :picture)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,5 @@ belongs_to :place #lesson 39
 class User < ActiveRecord::Base
 	mount_uploader :user, PictureUploader
 end
+
 end
